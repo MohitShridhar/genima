a38015e43a0b4fd6516696559cb7eb40f0733b4c
Mohit Shridhar <mohit.appi@gmail.com>
2024-07-10 17:25:14 +0100

    renmae

--- scripts/download_quickstart.sh
+++ scripts/download_quickstart.sh
@@ -3,3 +3,5 @@ cd ckpts
 wget https://github.com/MohitShridhar/genima/releases/download/1.0.0/25_tasks.zip
 unzip 25_tasks.zip
 rm 25_tasks.zip
+
+sed -i 's/tiger\.method/robobase.method/g' 25_tasks/controller_act/config.yaml
