f48b1ec8e7e4f1e02080e32dc9dfb472c77dcce5
Mohit Shridhar <mohit.appi@gmail.com>
2024-07-10 14:34:42 +0100

    update script

--- scripts/download_quickstart.sh
+++ scripts/download_quickstart.sh
@@ -1,5 +1,5 @@
 mkdir ckpts
 cd ckpts
-wget https://github.com/MohitShridhar/genima/releases/download/v1.0.0/25_tasks.zip
+wget https://github.com/MohitShridhar/genima/releases/download/1.0.0/25_tasks.zip
 unzip 25_tasks.zip
 rm 25_tasks.zip
